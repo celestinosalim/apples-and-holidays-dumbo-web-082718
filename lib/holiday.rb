dab45ccb07065ea19ea1c6db4b6e93837adbad5d
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-24 00:21:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,8 +18,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  4th = holiday_hash[:summer][1].to_s
-  4th
+ holiday_hash[:summer][1].to_s
+  binding.pry
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
