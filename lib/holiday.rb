c0ee5809e69db0691ad4315e22b032bbd3e9ed9c
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-24 00:29:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -9,12 +9,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  binding.pry
 
   winter_supplies(holiday_supplies)
-
-  end
-
+  binding.pry
 
 end
 
